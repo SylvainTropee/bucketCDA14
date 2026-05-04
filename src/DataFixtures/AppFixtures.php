331d810a1f0e8f0bd0032b8eaeb84da7c791c609
stropee <stropee@campus-eni.fr>
2026-05-04 14:45:17 +0200

    TP 7.2

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Wish;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
@@ -11,21 +13,50 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $this->addCategories($manager);
+        $this->addWishes($manager);
+
+    }
+
+    private function addCategories(ObjectManager $manager)
+    {
+        $categories = ['Travel & Adventure', 'Sport', 'Entertainment', 'Human Relations', 'Others'];
+
+        foreach ($categories as $cate){
+
+            $category = new Category();
+            $category->setName($cate);
+
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+
+
+
+    public function addWishes(ObjectManager $manager)
+    {
         $faker = Factory::create('fr_FR');
 
-        for($i = 0; $i < 100; $i++){
+        $categories = $manager->getRepository(Category::class)->findAll();
+
+        for ($i = 0; $i < 100; $i++) {
             $wish = new Wish();
             $wish
                 ->setDateCreated($faker->dateTimeBetween('-6 month'))
                 ->setAuthor($faker->name())
                 ->setTitle("Devenir " . $faker->jobTitle())
                 ->setIsPublished($faker->boolean(70))
-                ->setDescription($faker->paragraph(6));
+                ->setDescription($faker->paragraph(6))
+                ->setCategory($faker->randomElement($categories))
+            ;
 
             $manager->persist($wish);
-
-            }
-
+        }
         $manager->flush();
     }
+
+
+
 }
